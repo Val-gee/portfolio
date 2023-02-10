4bc5ece9eb550a8f19fae63bae05cf204d639efa
val.gee <vguevara0806@gmail.com>
2023-02-09 20:22:42 -0500

    finished styling for projects component

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -8,7 +8,7 @@ export default function Aboutme() {
                 <div className='col'>
                     <img src='./images/hpselfie2.png' className='photome' alt='Visiting The Harry Potter Store in NYC'></img>
                 </div>
-                <div className='col'>
+                <div className='col-6'>
                     <p id='abouttext'>
                         <strong>Hello visitor!</strong> Thank you for taking time to visit my portfolio. As a child growing up in Hollywood, Florida, I dedicated most of my time to school and synchronized
                         swimming practice. I was a part of the Aquacades traveling team for seven years. Those were some of the best years of my life. I owe a lot my teamwork and communication skills to the sport and my teammates. I learned to be
@@ -18,7 +18,7 @@ export default function Aboutme() {
                         Anthony, our poodle Chloe, our cat Bean, and our fish tanks. Anthony and I both enjoy outdoor activities and being a
                         part of our local Jiu Jitsu community at Radji Barret Jiu Jitsu in Fords. It has been a great
                         experience trying something new, aside from learning Web Development! Overall, I like to stay active
-                        and engage people in communities that share the same positive mindset!
+                        and engage people in communities that share the same positive mindset.
                     </p>
                 </div>
             </div>
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaGithub } from 'react-icons/fa'
 import './Projects.css';
 
 export default function Projects() {
@@ -6,22 +7,30 @@ export default function Projects() {
         <div >
             <div id='projects'>
                 <figure className="dimg card-column">
+                    <h3 className="card-header">
+                        The Journal Post
+                        <a href='https://github.com/jennahopeclem/self-journal' target="_blank"
+                            rel='noreferrer' style={{ color: "black", marginLeft: "14px" }}><FaGithub />
+                        </a>
+                    </h3>
                     <a className="link" target="_blank"
                         href="https://self-journal-post.herokuapp.com/" rel='noreferrer'>
                         <div className="card-body">
-                            <h3 className="card-header">
-                                The Journal Post
-                            </h3>
+                            <h4 className='projectDescription'>MVC Paradigm Application that uses RESTful API's, MySQL, and Handlebars</h4>
                         </div>
                     </a>
                 </figure>
                 <figure className="cimg card-column">
+                    <h3 className="card-header">
+                        Motivational Wep Page
+                        <a href='https://github.com/Val-gee/weekly-task-manager' target="_blank"
+                            rel='noreferrer' style={{ color: "black", marginLeft: "14px" }}><FaGithub />
+                        </a>
+                    </h3>
                     <a className="link" target="_blank"
                         href="https://val-gee.github.io/weekly-task-manager/" rel='noreferrer'>
                         <div className="card-body">
-                            <h3 className="card-header">
-                                Motivational Wep Page
-                            </h3>
+                            <h4 className='projectDescription'>JavaScript Application that uses server-side API's and client-side storage</h4>
                         </div>
                     </a>
                 </figure>
